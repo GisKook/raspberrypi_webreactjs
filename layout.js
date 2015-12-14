35f70adacdd18bda29c461d2c9721089d75aa98b
zhangkai <zhangkai.gis@163.com>
2015-12-14 16:46:36 +0800

    to do layout

---
+++ layout.js
@@ -0,0 +1,14 @@
+/**
+ * Created by z on 2015/12/14.
+ */
+$(window).load(function () {
+    //document.getElementById("show").style.left = document.documentElement.clientWidth/2 + "px";
+    document.getElementById("show").style.left = "500px";
+    document.getElementById("show").style.width = document.documentElement.clientWidth/2 + "px";
+    document.getElementById("show").style.height = document.documentElement.clientHeight + "px";
+
+    /*document.getElementById("menu").style.width = document.documentElement.clientWidth/2 + "px";
+    document.getElementById("menu").style.height = document.documentElement.clientHeight + "px";*/
+
+
+});
