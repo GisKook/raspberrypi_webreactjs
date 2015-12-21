dbb56ea5951918aca53cc20165dfd0eaf2121dbd
zhangkai <zhangkai.gis@163.com>
2015-12-21 16:47:28 +0800

    to use json to transform data

--- layout.js
+++ layout.js
@@ -2,13 +2,17 @@
  * Created by z on 2015/12/14.
  */
 $(window).load(function () {
-    //document.getElementById("show").style.left = document.documentElement.clientWidth/2 + "px";
-    document.getElementById("show").style.left = "500px";
+
+    var headerhight = document.getElementById("header").clientHeight;
+    $("body").css("overflow","hidden");
+
+    document.getElementById("menu").style.width = document.documentElement.clientWidth/2 + "px";
+    document.getElementById("menu").style.height = document.documentElement.clientHeight - headerhight+ "px";
+
+//    var msg = document.documentElement.clientWidth;
+//    window.alert(msg.toString());
     document.getElementById("show").style.width = document.documentElement.clientWidth/2 + "px";
-    document.getElementById("show").style.height = document.documentElement.clientHeight + "px";
-
-    /*document.getElementById("menu").style.width = document.documentElement.clientWidth/2 + "px";
-    document.getElementById("menu").style.height = document.documentElement.clientHeight + "px";*/
-
+    document.getElementById("show").style.height = document.documentElement.clientHeight - headerhight+ "px";
+    document.getElementById("show").style.backgroundColor="0x000000";
 
 });
